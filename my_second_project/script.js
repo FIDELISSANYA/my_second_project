155fc6f798071946e73903e09d4a626bde9435cc
FIDELIS SANYA <fidelis.sanya@student.moringaschool.com>
2022-03-04 20:48:29 +0300

    variables

--- my_second_project/script.js
+++ my_second_project/script.js
@@ -1,8 +1,12 @@
-let akanMaleName=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-let akanFemaleName=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-function akanName(){
-    var dateOfBirth=document.getElementById("date").value;
-    var monthofBirth=document.getElementById("month").value;
-    var yearOfBirth=document.getElementById("year").value;
-    var gender=document.getElementById("gd").value;
+let akanMaleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let akanFemaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+function akanName() {
+    var dateOfBirth = document.getElementById("date").value;
+    var monthofBirth = document.getElementById("month").value;
+    var yearOfBirth = document.getElementById("year").value;
+    var gender = document.getElementById("gd").value;
+
+    let birthDay = new Date("${date}-{month}-{year}");
+    var dateStr = birthDay.toDateString();
+    let biirthDate = birthDay.getDay();
 }
